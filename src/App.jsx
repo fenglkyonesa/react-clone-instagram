73db885c6560722ca53e6791ad48668dd4db42df
feng <1034013213@qq.com>
2024-04-17 08:05:15 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BottomNav } from "./components/Nav/BottomNav";
+import { SideNav } from "./components/Nav/SideNav";
+import TopNav from "./components/Nav/TopNav";
+
+export default function App() {
+  return (
+    <>
+      <div className=" w-full h-screen ">
+        <TopNav />
+        <div className="fixed hidden w-[80px] lg:w-[335px] light:bg-white md:block h-screen border-r-1  border-gray-800">
+          <SideNav />
+        </div>
+        <div className="fixed flex  md:hidden bottom-0 w-full gap-1 justify-space-between h-[50px] bg-black border-t-[1px] border-gray-700">
+          <BottomNav />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Nav/BottomNav.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {Avatar} from "@nextui-org/react";
+export const BottomNav = () => {
+  const buttons = [
+    {
+      src: "../public/navlogo/home.png",
+      link: "/",
+      alt: "home",
+    },
+    {
+      src: "../public/navlogo/explore.png",
+      link: "explore",
+      alt: "explore",
+    },
+    {
+      src: "../public/navlogo/reel.png",
+      link: "reel",
+      alt: "reel",
+    },
+    {
+      src: "../public/navlogo/create.png",
+      link: "create",
+      alt: "create",
+    },
+    {
+      src: "../public/navlogo/message.png",
+      link: "message",
+      alt: "message",
+    },
+  ];
+
+  return (
+    <>
+      {buttons.map((button, key) => {
+        return (
+          <Link
+            to={key === 0 ? "/" : `/${button.link}`}
+            key={key}           
+            className="flex w-2/12 items-center justify-center  border-gray-800 cursor-pointer hover:-translate-y-1 transition-all duration-300"
+          >
+            <img 
+            src={button.src} 
+            alt={button.alt} 
+            className="w-[30px] ">
+            </img>
+          </Link>
+        );
+      })}
+        <Link
+            to={"/"}
+            className="flex items-center justify-center  border-gray-800 cursor-pointer hover:-translate-y-1 transition-all duration-300"
+          >
+                  <Avatar
+                  size="sm"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+
+          </Link>
+    </>
+  );
+};
